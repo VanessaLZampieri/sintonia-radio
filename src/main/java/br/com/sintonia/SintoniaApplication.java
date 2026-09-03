b7ffbbe32f5b584130dae455afebbcb655d14d53
Vanessa Zampieri <vanessazampieri01@gmail.com>
2026-09-03 16:13:31 -0300

    feat: inicia projeto Sintonia

---
+++ src/main/java/br/com/sintonia/SintoniaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sintonia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SintoniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SintoniaApplication.class, args);
+    }
+
+}
